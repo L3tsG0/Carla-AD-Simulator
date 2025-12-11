afef0e83a4d5929179ff4b718f46cf18e84e2776
L3tsG0 <go@nsl.cs.waseda.ac.jp>
2025-12-11 09:54:49 +0900

    CARLAのシミュレータのオプションを追加

--- CarlaSimulator/run_carla_simulator.sh
+++ CarlaSimulator/run_carla_simulator.sh
@@ -9,4 +9,4 @@ docker run \
   -e NVIDIA_DRIVER_CAPABILITIES=all \
   --name carla-server \
   carlasim/carla:0.9.15 \
-  ./CarlaUE4.sh -RenderOffScreen -nosound -carla-port="$PORT"
+  ./CarlaUE4.sh -RenderOffScreen -nosound -carla-port="$PORT" -quality-level=Low -opengl
